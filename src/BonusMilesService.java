a0c3cce888c6915838e5b0ade49a4d2f4b1e8ee2
Neto work <lenas_2003@mail.ru>
2023-11-09 00:35:37 +0200

    Updated folder structure

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,10 @@
 public class BonusMilesService {
-    public int calculate(int price){
-        int miles;
-        if (price > 20) {
-            miles = price/20;
-        } else {
-            miles = price;
-        }
-        return miles;
-    }
 
+    public int calculate(int price) {
+        int rub = 20;
+        int bonusmiles = price / rub;
+
+
+    } return bonusmiles;
 }
+
